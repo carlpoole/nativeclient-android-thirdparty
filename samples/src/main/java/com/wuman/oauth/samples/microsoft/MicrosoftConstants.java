087cec55db2c9788b72d2408bd98f82c120323b9
Brandon Werner <brandwe@microsoft.com>
2016-02-27 01:08:18 -0800

    New sample for Microsoft

---
+++ samples/src/main/java/com/wuman/oauth/samples/microsoft/MicrosoftConstants.java
@@ -0,0 +1,26 @@
+package com.wuman.oauth.samples.microsoft;
+
+/**
+ * Created by brwerner on 2/26/16.
+ */
+/**
+ * Created by brwerner on 2/26/16.
+ */
+public class MicrosoftConstants {
+
+    public static final String CLIENT_ID = "bf52924d-a655-41a9-937c-08fda967e7e7";
+
+    public static final String CLIENT_SECRET = "ESNUTENGZB4Y31PKXM2GIQQMXPAG2TCLIRTFJ3QTV1YJ4F14";
+
+    public static final String AUTHORIZATION_CODE_SERVER_URL = "https://login.microsoftonline.com/kidventus.com/oauth2/v2.0/authorize";
+
+    public static final String AUTHORIZATION_IMPLICIT_SERVER_URL = "https://foursquare.com/oauth2/authenticate";
+
+    public static final String TOKEN_SERVER_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+
+    public static final String REDIRECT_URL = "urn:ietf:wg:oauth:2.0:oob";
+
+    private MicrosoftConstants() {
+    }
+
+}
---
+++ samples/src/main/java/com/wuman/oauth/samples/microsoft/MicrosoftScopes.java
@@ -0,0 +1,19 @@
+package com.wuman.oauth.samples.microsoft;
+
+/**
+ * Created by brwerner on 2/26/16.
+ */
+public class MicrosoftScopes {
+
+    /** Name, photo, headline, and current positions */
+    public static final String READ_CONTACTS = "https://outlook.office.com/Contacts.Read";
+
+    /**
+     * Full profile including experience, education, skills, and recommendations
+     */
+    public static final String READ_FULLPROFILE = "User.Read";
+
+
+    private MicrosoftScopes() {
+    }
+}
